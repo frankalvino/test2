dc38eb3cf1779dd91eae1ff1c6f7481a6dbdaaea
Frank Alvino <71982017+frankalvino@users.noreply.github.com>
2022-09-26 17:57:34 -0500

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,15 @@
+#comment my program to do arithmetic
+#n1 = 12
+#n2 = 10
+
+n1 = float(input("Enter a number"))
+n2 = float(input("Enter another number"))
+
+s = n1 + n2
+p = n1 * n2
+d = n1 - n2
+
+print("Sum is ", s)
+print("Difference is ", d)
+print("Product is " ,p)
+
